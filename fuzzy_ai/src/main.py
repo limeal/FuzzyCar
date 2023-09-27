363bcf686d32841041cbbff2274f5d23c9a885cd
BuildTools <paul.gazeau-rousseau@epitech.eu>
2023-09-27 17:42:26 +0900

    feat: Setup basic peer class python

--- fuzzy_ai/src/main.py
+++ fuzzy_ai/src/main.py
@@ -5,5 +5,25 @@
 ## main
 ##
 
+from network.peer import Peer   
+
+def main():
+    # Import Peer class from peer.py file
+    peer = Peer("0.0.0.0", 5432)
+    peer.start()
+    
+    while True:
+        command = input("Enter command: ")
+        if command == "connect":
+            peer.connect("0.0.0.0", 4242)
+        elif command == "send":
+            peer.send("Hello World!")
+        elif command == "quit":
+            break
+        else:
+            print("Invalid command")
+                
+
+
 if __name__ == '__main__':
-    print('Hello world!')
+    main()
